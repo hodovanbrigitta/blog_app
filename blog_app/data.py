de1a76392f604aba3534aa817d8a71d6212294f5
Brigitta Hodovan <hodovan.brigitta@gmail.com>
2019-11-25 18:54:13 +0100

    create tables

--- blog_app/data.py
+++ blog_app/data.py
@@ -1,3 +1,56 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+
+class AppUser(db.Model):
+    __tablename__ = 'app_user'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String, nullable=False)
+    user_password = db.Column(db.String, nullable=False)
+
+    blogs = db.relationship("Blog", back_populates="app_user")
+
+    comments = db.relationship("Comment", back_populates="app_user")
+
+    likes = db.relationship("Like", back_populates="app_user")
+
+
+class Blog(db.Model):
+    __tablename__ = 'blog'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String, nullable=False)
+    content = db.Column(db.String, nullable=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('app_user.id'))
+    app_user = db.relationship("AppUser", back_populates="blogs")
+
+    comments = db.relationship("Comment", back_populates="blog")
+
+    likes = db.relationship("Like", back_populates="app_user")
+
+
+class Comment(db.Model):
+    __tablename__ = 'comment'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    content = db.Column(db.String, nullable=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('app_user.id'))
+    app_user = db.relationship("AppUser", back_populates="comments")
+
+    blog_id = db.Column(db.Integer, db.ForeignKey('blog.id'))
+    blog = db.relationship("Blog", back_populates="comments")
+
+
+class Like(db.Model):
+    __tablename__ = 'like'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('app_user.id'))
+    app_user = db.relationship("AppUser", back_populates="likes")
+
+    blog_id = db.Column(db.Integer, db.ForeignKey('blog.id'))
+    blog = db.relationship("Blog", back_populates="likes")
