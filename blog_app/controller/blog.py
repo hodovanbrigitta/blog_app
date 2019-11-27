b16da635c337f22f9c2121959af6e5cbc951d2cb
Brigitta Hodovan <hodovan.brigitta@gmail.com>
2019-11-28 00:32:13 +0100

    implement update blog endpoint

--- blog_app/controller/blog.py
+++ blog_app/controller/blog.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, request, jsonify, make_response
+from flask import Blueprint, request, jsonify, make_response, g
+from werkzeug.exceptions import BadRequest
 
 from blog_app.controller import invalid_json_response
 from blog_app.data import Blog, db
@@ -20,19 +21,48 @@ def create_blog():
         blog = Blog(
             title=data["title"],
             content=data["content"],
-            user_id=data["user_id"]
+            user_id=g.user.get('user_id')
         )
     except KeyError as e:
         return invalid_json_response(f"missing property: {e}")
-    user_in_db = get_user_by_id(data["user_id"])
-    if not user_in_db:
-        return invalid_json_response(f"user with id {data['user_id']} does not exist")
     db.session.add(blog)
     db.session.commit()
     db.session.refresh(blog)
     blog_resource = jsonify({
+        "id": blog.id,
         "title": blog.title,
         "content": blog.content,
         "user_id": blog.user_id,
     })
     return make_response(blog_resource, 201)
+
+
+@blog_api.route("/blog/<int:blog_id>", methods=["PUT"])
+@Auth.auth_required
+def update_blog(blog_id):
+    """
+    Update a blog
+
+    :return: the updated blog
+    """
+    try:
+        data = request.get_json()
+    except BadRequest:
+        return invalid_json_response("invalid request body")
+
+    blog = db.session.query(Blog).filter(Blog.id == blog_id).first()
+    if blog is None:
+        return invalid_json_response(
+            f"recipe with ID {blog_id} does not exist")
+    if blog.user_id != g.user.get('user_id'):
+        return invalid_json_response("permission denied")
+    blog.title = data["title"]
+    blog.content = data["content"]
+    db.session.commit()
+    db.session.refresh(blog)
+    return jsonify({
+        "id": blog.id,
+        "title": blog.title,
+        "content": blog.content,
+        "user_id": blog.user_id
+    })
