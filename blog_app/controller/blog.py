1366a78f161e51543cb5d78bee1422a7f204de48
Brigitta Hodovan <hodovan.brigitta@gmail.com>
2019-11-28 15:15:36 +0100

    implement delete blog endpoint

--- blog_app/controller/blog.py
+++ blog_app/controller/blog.py
@@ -8,6 +8,42 @@ from blog_app.service.app_user_service import get_user_by_id, Auth
 blog_api = Blueprint('blog', __name__)
 
 
+@blog_api.route("/blog", methods=["GET"])
+def get_blogs():
+    """
+    Returns every blog from our application
+
+    :return: every blog from our application
+    """
+    blog_models = db.session.query(Blog).all()
+    return jsonify([{
+        "id": blog.id,
+        "title": blog.title,
+        "content": blog.content,
+        "user_id": blog.user_id
+    } for blog in blog_models])
+
+
+@blog_api.route("/blog/<int:blog_id>", methods=["GET"])
+def get_category(blog_id):
+    """
+    Returns a blog from our application with the given ID
+
+    :param blog_id: the blog's ID to return
+    :return: a blog from our application with the given ID
+    """
+    blog = db.session.query(Blog).filter(Blog.id == blog_id).first()
+    if blog is None:
+        return invalid_json_response(f"blog with ID {blog_id} does not exist")
+
+    return jsonify({
+        "id": blog.id,
+        "title": blog.title,
+        "content": blog.content,
+        "user_id": blog.user_id
+    })
+
+
 @blog_api.route("/blog", methods=["POST"])
 @Auth.auth_required
 def create_blog():
@@ -66,3 +102,22 @@ def update_blog(blog_id):
         "content": blog.content,
         "user_id": blog.user_id
     })
+
+
+@blog_api.route("/blog/<int:blog_id>", methods=["DELETE"])
+@Auth.auth_required
+def delete_blog(blog_id):
+    """
+    Delete a blog from our application
+
+    :return: 204 No Content
+    """
+    blog = db.session.query(Blog).filter(Blog.id == blog_id).first()
+    if not blog:
+        return invalid_json_response("blog not found")
+    if blog.user_id != g.user.get('user_id'):
+        return invalid_json_response("permission denied")
+    if blog:
+        db.session.delete(blog)
+        db.session.commit()
+    return '', 204
