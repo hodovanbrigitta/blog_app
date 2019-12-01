3e42481a21ab81f49e98bc006c207211207e1737
Brigitta Hodovan <hodovan.brigitta@gmail.com>
2019-12-01 20:48:33 +0100

    implement get me endpoint and fix auth

--- blog_app/controller/app_user.py
+++ blog_app/controller/app_user.py
@@ -1,11 +1,11 @@
-from flask import json
+from flask import Blueprint, request, jsonify, make_response, g
 
-from flask import Blueprint, request, jsonify, make_response, Response
-
-from blog_app.controller import invalid_json_response, bcrypt, save
-from blog_app.data import AppUser, db
+from blog_app.controller import invalid_json_response
+from blog_app.service import save
 from blog_app.service.app_user_service import get_user_by_username, \
-    __generate_hash, Auth, check_hash, get_all_users
+    generate_hash, check_hash, get_all_users, get_user_by_id, \
+    create_user
+from blog_app.service.authentication import auth
 
 user_api = Blueprint('app_user', __name__)
 
@@ -25,6 +25,23 @@ def get_app_users():
     } for app_user in user_models])
 
 
+@user_api.route("/app_user/me", methods=["GET"])
+def get_me():
+    """
+    Returns the basic data about the actual user
+    :return: a user from our application
+    """
+    app_user = get_user_by_id(g.user.get('user_id'))
+    if app_user is None:
+        return invalid_json_response(
+            f"user with ID {g.user.get('user_id')} does not exist")
+    return jsonify({
+        "id": app_user.id,
+        "app_username": app_user.app_username,
+        "user_password": app_user.user_password
+    })
+
+
 @user_api.route("/app_user/<user_id>", methods=["GET"])
 def get_app_user(user_id):
     """
@@ -37,7 +54,7 @@ def get_app_user(user_id):
         user_id = int(user_id)
     except ValueError:
         return invalid_json_response("invalid input type")
-    app_user = db.session.query(AppUser).filter(AppUser.id == user_id).first()
+    app_user = get_user_by_id(user_id)
     if app_user is None:
         return invalid_json_response(
             f"user with ID {user_id} does not exist")
@@ -49,7 +66,7 @@ def get_app_user(user_id):
 
 
 @user_api.route("/app_user", methods=["POST"])
-def create_user():
+def create_new_user():
     """
     Creates a user
 
@@ -57,18 +74,17 @@ def create_user():
     """
     data = request.get_json()
     try:
-        app_user = AppUser(
-            app_username=data["app_username"],
-            user_password=__generate_hash(data["user_password"])
-        )
+        app_user = create_user(data["app_username"],
+                               generate_hash(data["user_password"]))
     except KeyError as e:
         return invalid_json_response(f"missing property: {e}")
     user_in_db = get_user_by_username(data["app_username"])
 
     if user_in_db:
-        return invalid_json_response(f"User: {data['app_username']} already exist")
+        return invalid_json_response(f"User: {data['app_username']} "
+                                     f"already exist")
     save(app_user)
-    token = Auth.generate_token(app_user.id)
+    token = auth.generate_token(app_user.id)
     res_token = jsonify({
         "jwt-token": token,
         "id": app_user.id,
@@ -82,23 +98,21 @@ def create_user():
 def login_user():
     data = request.get_json()
     try:
-        actual_user = AppUser(
-            app_username=data["app_username"],
-            user_password=data["user_password"]
-        )
+        actual_user = create_user(data["app_username"], data["user_password"])
     except KeyError as e:
         return invalid_json_response(f"missing property: {e}")
 
     user_in_db = get_user_by_username(actual_user.app_username)
 
     if not user_in_db:
+        # TODO do not expose existing usernames
         return invalid_json_response(
             f"user with username {data['app_username']} does not exist")
 
     if not check_hash(user_in_db.user_password, data["user_password"]):
         return invalid_json_response(f"invalid password")
 
-    token = Auth.generate_token(user_in_db.id)
+    token = auth.generate_token(user_in_db.id)
 
     res_token = jsonify({
         "jwt-token": token,
@@ -107,8 +121,3 @@ def login_user():
         "user_password": user_in_db.user_password
     })
     return make_response(res_token, 200)
-
-
-
-
-
