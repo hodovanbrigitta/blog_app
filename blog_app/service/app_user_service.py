4923c08cf05750fa35f52a14b32f83325090cb9a
Brigitta Hodovan <hodovan.brigitta@gmail.com>
2019-12-01 20:50:22 +0100

    move Auth class to authentication.py

--- blog_app/service/app_user_service.py
+++ blog_app/service/app_user_service.py
@@ -1,27 +1,18 @@
-import datetime
-import os
-from functools import wraps
-
-import jwt
-from flask import Response, request, current_app, g
-
-from blog_app.controller import bcrypt, invalid_json_response
 from blog_app.data import AppUser, db
+from blog_app.service import bcrypt
 
-from flask import json
 
-
-def __generate_hash(password):
+def generate_hash(password):
     return bcrypt.generate_password_hash(password, rounds=10).decode("utf-8")
 
 
 def check_hash(pw_hash, password):
-    #xx = check_hash(user_in_db.user_password, data["user_password"])
     return bcrypt.check_password_hash(pw_hash, password)
 
 
 def get_user_by_username(username):
-    return db.session.query(AppUser).filter(AppUser.app_username == username).first()
+    return db.session.query(AppUser)\
+        .filter(AppUser.app_username == username).first()
 
 
 def get_user_by_id(user_id):
@@ -32,70 +23,9 @@ def get_all_users():
     return db.session.query(AppUser).all()
 
 
-class Auth:
-    @staticmethod
-    def generate_token(user_id):
-        """
-        Generate Token Method
-        """
-        try:
-            payload = {
-                'exp': datetime.datetime.utcnow() + datetime.timedelta(days=1),
-                'iat': datetime.datetime.utcnow(),
-                'sub': user_id
-            }
-            return jwt.encode(payload, current_app.config['JWT_SECRET_KEY'], algorithm='HS256').decode("utf-8")
-        except Exception as e:
-            return invalid_json_response(f" error in generating user token: {e}")
+def create_user(username, user_password):
+    return AppUser(
+        app_username=username,
+        user_password=user_password
+    )
 
-    @staticmethod
-    def decode_token(token):
-        """
-        Decode token method
-        """
-        re = {'data': {}, 'error': {}}
-        try:
-            payload = jwt.decode(token, current_app.config['JWT_SECRET_KEY'])
-            re['data'] = {'user_id': payload['sub']}
-            return re
-        except jwt.ExpiredSignatureError as e1:
-            re['error'] = {'message': 'token expired, please login again'}
-            return re
-        except jwt.InvalidTokenError:
-            re['error'] = {
-                'message': 'Invalid token, please try again with a new token'}
-            return re
-
-    @staticmethod
-    def auth_required(func):
-        """
-        Auth decorator
-        """
-        @wraps(func)
-        def decorated_auth(*args, **kwargs):
-            if 'api-token' not in request.headers:
-                return invalid_json_response('Authentication token is not '
-                                             'available, please login to '
-                                             'get one')
-            token = request.headers.get('api-token')
-            data = Auth.decode_token(token)
-            if data['error']:
-                return Response(
-                    mimetype="application/json",
-                    response=json.dumps(data['error']),
-                    status=400
-                )
-
-            user_id = data['data']['user_id']
-            check_user = get_user_by_id(user_id)
-            if not check_user:
-                return Response(
-                    mimetype="application/json",
-                    response=json.dumps(
-                        {'error': 'user does not exist, invalid token'}),
-                    status=400
-                )
-            g.user = {'user_id': user_id}
-            return func(*args, **kwargs)
-
-        return decorated_auth
---
+++ blog_app/service/authentication.py
@@ -0,0 +1,81 @@
+import datetime
+from functools import wraps
+
+import jwt
+from flask import Response, request, current_app, g
+from flask import json
+
+from blog_app.controller import invalid_json_response
+from blog_app.service.app_user_service import get_user_by_id
+
+
+class Authentication:
+    def generate_token(self, user_id):
+        """
+        Generate Token Method
+        """
+        try:
+            payload = {
+                'exp': datetime.datetime.utcnow() + datetime.timedelta(days=1),
+                'iat': datetime.datetime.utcnow(),
+                'sub': user_id
+            }
+            return jwt.encode(payload, current_app.config['JWT_SECRET_KEY'],
+                              algorithm='HS256').decode("utf-8")
+        except Exception as e:
+            return invalid_json_response(
+                f" error in generating user token: {e}")
+
+    def decode_token(self, token):
+        """
+        Decode token method
+        """
+        re = {'data': {}, 'error': {}}
+        try:
+            payload = jwt.decode(token, current_app.config['JWT_SECRET_KEY'])
+            re['data'] = {'user_id': payload['sub']}
+            return re
+        except jwt.ExpiredSignatureError:
+            re['error'] = {'message': 'token expired, please login again'}
+            return re
+        except jwt.InvalidTokenError:
+            re['error'] = {
+                'message': 'Invalid token, please try again with a new token'}
+            return re
+
+    def auth_required(self, func):
+        """
+        Auth decorator
+        """
+        @wraps(func)
+        def decorated_auth(*args, **kwargs):
+            if 'api-token' not in request.headers:
+                return invalid_json_response('Authentication token is not '
+                                             'available, please login to '
+                                             'get one')
+            token = request.headers.get('api-token')
+            data = auth.decode_token(token)
+            if data['error']:
+                return Response(
+                    mimetype="application/json",
+                    response=json.dumps(data['error']),
+                    status=400
+                )
+
+            user_id = data['data']['user_id']
+            check_user = get_user_by_id(user_id)
+            if not check_user:
+                return Response(
+                    mimetype="application/json",
+                    response=json.dumps(
+                        {'error': 'user does not exist, invalid token'}),
+                    status=400
+                )
+            g.user = {'user_id': user_id}
+            return func(*args, **kwargs)
+
+        return decorated_auth
+
+
+auth = Authentication()
+
